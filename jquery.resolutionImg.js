722b86f2f28d514f95751fef0f7a6a727be34475
muhammetus <muhammetuss@gmail.com>
2014-02-21 14:04:18 +0200

    add files

---
+++ jquery.resolutionImg.js
@@ -0,0 +1,22 @@
+(function ( $ ) {
+    $.fn.resolutionImg = function(options) {
+         
+		var wWidth  = $(window).width(),
+			showImage = null,
+			showTag   = null,
+			selector = this.selector;
+			
+			$.each(options,function(key,value){
+				//last resolution
+				 if(key <=  wWidth){
+					showImage = key;
+					showTag   = value;		
+				}
+			});
+			$(selector + ' img['+showTag+']').each(function(){
+				this.src = $(this).attr(showTag);
+			});
+			
+			//setTimeOut
+    };
+}( jQuery ));
